c5b10d13e84c18f25a173a84e681ea2ec8b7b873
Sixto Ortega <sixto_miguel@hotmail.com>
2019-10-25 14:49:47 -0400

    Upload project

---
+++ SixtoUploadFilesProgress/Controllers/UploadController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SixtoUploadFilesProgress.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+
+        public UploadController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(List<IFormFile> files)
+        {
+            string fileNameWithExt = "";
+            var filePath = $"{_env.WebRootPath}\\Attached\\";
+            var DirInfo = new System.IO.DirectoryInfo(filePath);
+            if (DirInfo.Exists == false) DirInfo.Create();
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var formFile in files)
+                {
+                    //var formFile = files[0];
+
+                    if (formFile != null && formFile.Length > 0)
+                    {
+                        System.IO.FileInfo myFile = new System.IO.FileInfo(formFile.FileName);
+                        fileNameWithExt = $"{DateTime.Now.ToString("MMddyyyy")}_{DateTime.Now.ToString("HHmmss")}_{myFile.Name}";
+                        filePath += fileNameWithExt;
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(stream);
+                        }
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SixtoUploadFilesProgress/wwwroot/js/upload-files-indicator.js
@@ -0,0 +1,43 @@
+﻿function submitFormUploadFile(url, input_file, id_tag_html_indicator) {
+
+    var xhr = new XMLHttpRequest()
+
+    xhr.upload.onprogress = function (e) {
+        if (e.lengthComputable) {
+            var percentComplete = parseInt((e.loaded / e.total) * 100);
+            updateValueUploadIndicator(id_tag_html_indicator, percentComplete)
+            console.log(percentComplete + '% uploaded')
+        }
+    };
+
+    xhr.upload.onloadstart = function (e) {
+        console.log('Uploaded Start')
+    }
+
+    xhr.upload.onloadend = function (e) {
+        console.log('Uploaded End')
+    }
+
+    xhr.upload.onerror = function (e) {
+        updateValueUploadIndicator(id_tag_html_indicator, 'ERROR')
+        console.log(e)
+    }
+
+    xhr.onload = function () {
+        if (this.status == 200) {
+            updateValueUploadIndicator(id_tag_html_indicator, 'Success')
+            console.log('Uploaded Completed')
+        };
+    };
+
+    xhr.open('POST', url, true)
+    var formData = new FormData()
+    for (var i = 0; i < input_file.files.length; i++) {
+        formData.append("files", input_file.files[i])
+    }
+    xhr.send(formData)
+}
+
+function updateValueUploadIndicator(id_tag_html_indicator, valueHtml) {
+    document.getElementById(id_tag_html_indicator).innerHTML = valueHtml
+}
